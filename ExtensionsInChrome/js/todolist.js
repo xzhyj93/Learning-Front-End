2c756461fc41a47e38ddd52829689ad8dce13a50
hao <xzhyj93@sohu.com>
2017-03-07 14:59:56 +0800

    update 2017-03-07 使用localStorage更新代码, 使得备忘录可保存在本地

--- ExtensionsInChrome/js/todolist.js
+++ ExtensionsInChrome/js/todolist.js
@@ -1,15 +1,34 @@
 var todoapp = window.angular.module("todoapp", [])
+var key = "todoLists"
+
+//把nodelist数组转换成储存进localStorage的字符串
+function listToStr(list){
+    var strList = []
+    for(item in list){
+        strList.push("{\"text\": \""+list[item].text + "\", \"done\": \""+list[item].done +"\"}")
+    }
+    return (strList.join("&&&"))
+}
 
 todoapp.controller('todolistcontroller', ['$scope', function($scope){
     $scope.text = '';
-
-    $scope.todolist = [{
-        text: 'hello world',
-        done: false
-    },{
-        text: 'I use Python',
-        done: false
-    }];
+    var todoLists = localStorage.getItem(key)
+    if(!todoLists){
+        $scope.todolist = []
+    }
+    else {
+        var list = todoLists.split('&&&')
+        $scope.todolist = []
+        for(item in list){
+            aaa = list[item]
+            var jsonItem = JSON.parse(list[item])
+            // console.log(list[item])
+            $scope.todolist.push({
+                text: jsonItem.text,
+                done: jsonItem.done
+            })
+        }
+    }
 
     $scope.add = function(){
         var text = $scope.text.trim();
@@ -18,17 +37,25 @@ todoapp.controller('todolistcontroller', ['$scope', function($scope){
                 text: text,
                 done: false
             });
-            $socpe.text = '';
+            $scope.text = '';
+            localStorage.setItem(key,listToStr($scope.todolist))
         }
     }
     $scope.delete = function(todo){
         var index = $scope.todolist.indexOf(todo);
         $scope.todolist.splice(index, 1);
+        localStorage.setItem(key,listToStr($scope.todolist))
     }
     $scope.doneCount = function(){
         var temp = $scope.todolist.filter(function(item){
             return item.done
         });
         return temp.length
+    }
+    //当任务完成时也要更新localStorage
+    $scope.update = function(item){
+        console.log($scope.todolist)
+        localStorage.setItem(key,listToStr($scope.todolist))
+
     }
 }]);
